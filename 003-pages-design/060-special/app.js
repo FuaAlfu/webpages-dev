3e6d545af28b7be889c0266ed446f0794f639b20
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-22 00:25:01 +0300

    add

---
+++ 003-pages-design/060-special/app.js
@@ -0,0 +1,9 @@
+const date = () => {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+
+    document.getElementById('d').innerHTML = currentYear.toString();
+    
+}
+
+date();
